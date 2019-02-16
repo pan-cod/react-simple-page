62d78d8871ae4b32cc70a8f6708037a3a03fd0e0
PANCOD Dariusz Markowicz <hello@pancod.com>
2019-02-16 17:34:27 +0100

    cleaning starter kit

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import style from './styles/app.scss';
 
-const App = () => <div className={style.app} />;
+const App = () => (
+  <div className={style.oWrapper}>
+    {/* <Header />
+    <Main />
+    <Footer /> */}
+  </div>
+);
 
 export default hot(module)(App);
